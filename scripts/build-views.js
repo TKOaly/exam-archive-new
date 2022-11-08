5cb0ea27fb52d428be9feea3d1913134e9226a82
Tuomas Alanen <tuomas.alanen@reaktor.com>
2022-11-08 21:28:32 +0200

    run prettier

--- scripts/build-views.js
+++ scripts/build-views.js
@@ -1,32 +1,32 @@
-const chokidar = require('chokidar');
+const chokidar = require('chokidar')
 const fs = require('fs/promises')
 const path = require('path')
 
-const buildDir = path.join(__dirname, "..", "src", "views")
-const destinationDir = path.join(__dirname, "..", "dist", "views")
+const buildDir = path.join(__dirname, '..', 'src', 'views')
+const destinationDir = path.join(__dirname, '..', 'dist', 'views')
 
-const getFileList = async (dir) => {
-    let files = []
-    const items = await fs.readdir(dir, { withFileTypes: true })
+const getFileList = async dir => {
+  let files = []
+  const items = await fs.readdir(dir, { withFileTypes: true })
 
-    for (const item of items) {
-        if (item.isDirectory()) {
-            const filesFromDir = await getFileList(`${dir}/${item.name}`)
-            files = [...files, ...filesFromDir]
-        } else {
-            files = [...files, `${dir}/${item.name}`]
-        }
+  for (const item of items) {
+    if (item.isDirectory()) {
+      const filesFromDir = await getFileList(`${dir}/${item.name}`)
+      files = [...files, ...filesFromDir]
+    } else {
+      files = [...files, `${dir}/${item.name}`]
     }
+  }
 
-    return files
+  return files
 }
 
 const copyFiles = async () => {
-    const files = await getFileList(buildDir)
-    files.forEach(async (file) => {
-        console.log(`Copying ${file.replace(buildDir, "")} to dist/views`)
-        await fs.cp(file, `${destinationDir}/${file.replace(buildDir, "")}`)
-    })
+  const files = await getFileList(buildDir)
+  files.forEach(async file => {
+    console.log(`Copying ${file.replace(buildDir, '')} to dist/views`)
+    await fs.cp(file, `${destinationDir}/${file.replace(buildDir, '')}`)
+  })
 }
 
 copyFiles()

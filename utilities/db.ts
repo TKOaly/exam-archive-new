042a3dc3733a83a9b03b59a5f9ff66a27f241caa
Tuomas Alanen <tuomas@alanen.dev>
2023-05-09 11:56:55 +0300

    Define DB pool for node-postgres

--- utilities/db.ts
+++ utilities/db.ts
@@ -1,5 +1,6 @@
 import knex from 'knex'
-import { knexConfig } from '@utilities/config'
+import { Pool, Client } from 'pg'
+import config, { knexConfig } from '@utilities/config'
 
 interface KnexTimestamped {
   created_at: number
@@ -25,3 +26,20 @@ export const db = knex(knexConfig[process.env.NODE_ENV])
 export const testConnection = async () => {
   await db('courses').count('id')
 }
+
+let dbPool: Pool
+
+if (process.env.NODE_ENV === 'production') {
+  dbPool = new Pool({
+    connectionString: config.PG_CONNECTION_STRING
+  })
+} else {
+  if (!global.dbPool) {
+    global.dbPool = new Pool({
+      connectionString: config.PG_CONNECTION_STRING
+    })
+  }
+  dbPool = global.dbPool
+}
+
+export { dbPool }

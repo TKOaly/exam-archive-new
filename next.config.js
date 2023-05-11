6ee39b5f09f91f659c1d9743116b4a0e91157bce
Tuomas Alanen <tuomas@alanen.dev>
2023-05-11 18:05:37 +0300

    Add security headers

--- next.config.js
+++ next.config.js
@@ -8,6 +8,37 @@ const nextConfig = {
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              'microphone=(), geolocation=(), usb=(), payment=(), autoplay=(), battery=(), display-capture=(), document-domain=(), encrypted-media=(), gamepad=(), gyroscope=(), midi=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=()'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; img-src 'self'; media-src 'self'; script-src 'self';"
+          }
+        ]
+      }
+    ]
   }
 }
 

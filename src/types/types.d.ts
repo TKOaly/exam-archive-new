46a692620a05221e2ad5bbbcb7c06e48ecbd5b22
Tuomas Alanen <tuomas@alanen.dev>
2023-05-11 18:12:08 +0300

    Cleanup old stuff

--- src/types/types.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Express {
-  export interface Request {
-    flash(msg: string | undefined, type: 'error' | 'info' | undefined): void
-    flash(): { msg: string; type: 'error' | 'info' } | undefined
-  }
-}

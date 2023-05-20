bd5975eb401e94a1a06c3595feb3108822e68210
Tuomas Alanen <tuomas@alanen.dev>
2023-05-20 12:18:59 +0300

    Refactor session handling to be more usable

--- lib/sessions.ts
+++ lib/sessions.ts
@@ -6,23 +6,37 @@ import { redirect } from 'next/navigation'
 import { AccessRight, AuthData, UserServiceUser } from './types'
 import { NextApiRequest } from 'next'
 
-export const getSession = async (): Promise<AuthData> => {
+export const getSessionCookie = () => {
   const cookiesStore = cookies()
   const session = cookiesStore.get(config.COOKIE_NAME)
 
   if (!session) {
-    redirect(getUserServiceLoginUrl())
+    return null
   }
 
-  const { payload } = await jwtDecrypt(session.value, config.COOKIE_SECRET, {
+  return session.value
+}
+
+export const decryptSession = async (session: string) => {
+  const { payload } = await jwtDecrypt(session, config.COOKIE_SECRET, {
     issuer: config.COOKIE_ISSUER,
     subject: config.COOKIE_SUBJECT
   })
-
   return {
     user: payload.user as UserServiceUser,
     rights: payload.rights as { [right in AccessRight]: boolean }
   }
+}
+
+export const getSession = async (): Promise<AuthData> => {
+  const session = getSessionCookie()
+
+  if (!session) {
+    console.log('No session found, redirecting to login')
+    redirect(getUserServiceLoginUrl())
+  }
+
+  return decryptSession(session)
 }
 
 export const getLegacyApiSession = async (
@@ -37,13 +51,5 @@ export const getLegacyApiSession = async (
     return null
   }
 
-  const { payload } = await jwtDecrypt(session, config.COOKIE_SECRET, {
-    issuer: config.COOKIE_ISSUER,
-    subject: config.COOKIE_SUBJECT
-  })
-
-  return {
-    user: payload.user as UserServiceUser,
-    rights: payload.rights as { [right in AccessRight]: boolean }
-  }
+  return decryptSession(session)
 }

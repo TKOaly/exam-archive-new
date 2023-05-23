750a89c3dbce301689a4941e54dcf0d6ff39cf54
Tuomas Alanen <tuomas@alanen.dev>
2023-05-23 11:26:09 +0300

    Convert upload exam to server components and allow multiple files to be uploaded at the same time
    aws-sdk/client-s3 and its putobjectcommandinputs body were horror to find out in which format it would like to have file object coming from formdata

--- components/tools/UploadExam.tsx
+++ components/tools/UploadExam.tsx
@@ -1,65 +1,93 @@
-'use client'
-import { useState, MouseEvent } from 'react'
-import { useRouter } from 'next/navigation'
+import { PutObjectCommand, PutObjectCommandInput } from '@aws-sdk/client-s3'
+import { getCourseInfo, createExam } from '@services/archive'
+import s3 from '@services/s3'
+import { validateRights } from '@services/tkoUserService'
+import contentDisposition from 'content-disposition'
+import { randomUUID as uuidv4 } from 'crypto'
+import { transliterate } from 'transliteration'
+import configs from '@lib/config'
+import { urlForCourse } from '@lib/courses'
+import { revalidatePath } from 'next/cache'
+import { getSession } from '@lib/sessions'
 
 interface UploadExamProps {
   courseId: number
 }
 
 const UploadExam = ({ courseId }: UploadExamProps) => {
-  const [file, setFile] = useState<File | null>(null)
-  const router = useRouter()
+  const uploadExam = async (formData: FormData) => {
+    'use server'
+    const { rights } = await getSession()
 
-  const uploadExam = async (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
+    const isRights = validateRights(rights, 'upload')
+    if (!isRights) {
+      return 'Unauthorized'
+    }
+    const courseId = parseInt(formData.get('courseId') as string, 10) // TODO: make better type check
 
-    if (!file) {
-      alert('Please select a file to upload')
-      return
+    const course = await getCourseInfo(courseId)
+    if (course === null) {
+      return 'Cannot upload a file to a course that does not exist.'
     }
 
-    const body = new FormData()
-    body.append('file', file)
-    body.append('courseId', courseId.toString())
+    const files = [...formData.values()]
+      .filter(entry => entry instanceof File)
+      .map(entry => entry as File)
 
-    const request = await fetch('/api/exam/upload', {
-      method: 'POST',
-      body,
-      credentials: 'same-origin'
-    })
+    const exams = await Promise.all(
+      files.map(async file => {
+        const originalFilename = file.name as string
+        const contentType = file.type as string
 
-    const response = await request.json()
+        const params: PutObjectCommandInput = {
+          Bucket: configs.AWS_S3_BUCKET_ID,
+          Key: uuidv4(),
+          ContentDisposition: contentDisposition(originalFilename, {
+            type: 'inline',
+            fallback: transliterate(originalFilename)
+          }),
+          ContentType: contentType,
+          ACL: 'private',
+          ContentLength: file.size,
+          Body: Buffer.from(await file.arrayBuffer())
+        }
 
-    if (!request.ok) {
-      alert(response.error)
-      return
-    }
+        await s3.send(new PutObjectCommand(params))
 
-    router.refresh()
+        const exam = await createExam({
+          courseId: course.id,
+          fileName: originalFilename,
+          filePath: params.Key as string,
+          mimeType: contentType
+        })
+
+        return exam
+      })
+    )
+    revalidatePath(urlForCourse(course.id, course.name))
   }
 
   return (
     <div className="exam-upload-form">
       <h3>Upload a new file here:</h3>
-      <input
-        className="exam-upload-form__file"
-        onChange={e => {
-          if (!e.target.files) return
-          setFile(e.target.files[0])
-        }}
-        aria-label="File"
-        type="file"
-        name="file"
-      />
-      <button
-        className="exam-upload-form__submit"
-        type="submit"
-        name="upload"
-        value="Upload"
-        onClick={uploadExam}
-      >
-        Upload
-      </button>
+      <form action={uploadExam}>
+        <input
+          className="exam-upload-form__file"
+          aria-label="File"
+          type="file"
+          name="file"
+          multiple
+        />
+        <input hidden name="courseId" defaultValue={courseId} />
+        <button
+          className="exam-upload-form__submit"
+          type="submit"
+          name="upload"
+          value="Upload"
+        >
+          Upload
+        </button>
+      </form>
     </div>
   )
 }

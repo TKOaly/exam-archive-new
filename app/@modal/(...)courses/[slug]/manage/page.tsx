65d014eff624b14c3dd1434d227341545b32022f
Tuomas Alanen <tuomas@alanen.dev>
2023-07-20 21:56:33 +0300

    Create course management modal implementation

---
+++ app/@modal/(...)courses/[slug]/manage/page.tsx
@@ -0,0 +1,57 @@
+import { notFound, redirect } from 'next/navigation'
+
+// import { slugifyCourseName, urlForCourse } from '@lib/courses'
+import { getSession } from '@lib/sessions'
+import { getCourseInfo } from '@services/archive'
+
+import Modal from '@components/Modal'
+import RenameCourse from '@components/tools/RenameCourse'
+import DeleteCourse from '@components/tools/DeleteCourse'
+
+const parseSlug = (slug: string) => {
+  const parsedSlug = slug.match(/(?<id>\d+)-(?<courseSlug>.*)/)
+  if (!parsedSlug || !parsedSlug.groups) {
+    notFound()
+  }
+
+  const id = parseInt(parsedSlug.groups.id, 10)
+
+  if (isNaN(id)) {
+    notFound()
+  }
+
+  return {
+    id,
+    courseSlug: parsedSlug.groups.courseSlug
+  }
+}
+
+const Page = async ({ params }: any) => {
+  const { rights } = await getSession()
+
+  if (!rights.rename || !rights.remove) {
+    redirect('/')
+  }
+
+  const { id, courseSlug } = parseSlug(params.slug)
+
+  const course = await getCourseInfo(id)
+  if (!course) {
+    notFound()
+  }
+
+  // // if (courseSlug !== slugifyCourseName(course.name)) {
+  // //   return redirect(urlForCourse(course.id, course.name))
+  // // }
+
+  return (
+    <Modal title={`Manage course "${course.name}"`}>
+      <div className="flex flex-col gap-8">
+        <RenameCourse courseId={course.id} currentName={course.name} />
+        <DeleteCourse courseId={course.id} courseName={course.name} />
+      </div>
+    </Modal>
+  )
+}
+
+export default Page

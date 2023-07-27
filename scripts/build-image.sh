d66e8d6f77d84257f9e5e4aca0725b3393e3ef7a
Tuomas Alanen <tuomas@alanen.dev>
2023-07-27 17:52:11 +0300

    Add forgotten DB setup

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+export COMPOSE_PROJECT_NAME="exam-archive-new-build"
 readonly repository="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)"
 source "$repository/scripts/common.sh"
 
 function main() {
+    required_command npm
+    required_command docker
+    required_command docker-compose
 
     pushd "$repository"
+
+    echo "::group::Installing node and dependencies"
+    check_node_version
+    npm_ci
+    echo "::endgroup::"
+
+    echo "::group::Starting database and S3"
+    docker-compose up -d db s3
+
+    db_health_check
+    s3_health_check
+    echo "::endgroup::"
 
     export DOCKER_TAGS=${DOCKER_TAGS:-"tarpisto/tarpisto:latest"}
     export DOCKER_LABELS=${DOCKER_LABELS:-""}

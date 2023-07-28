718f044b3ee5378dcafa3fb0f23ff3efc1641c1c
Tuomas Alanen <tuomas@alanen.dev>
2023-07-28 17:53:02 +0300

    Check error and not found page styling

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -12,12 +12,12 @@ const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
   }, [error])
 
   return (
-    <div className="page-container">
-      <main>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
-      </main>
-    </div>
+    <main>
+      <div className="flex flex-col gap-4 items-center bg-white py-10">
+        <h2 className="mx-1 my-2 shrink font-serif text-3xl font-extrabold leading-tight">Something went wrong!</h2>
+        <button className="bg-gray-800 hover:bg-gray-600 focus:ring focus:ring-gray-400 ring-inset text-white font-serif lowercase p-3 box-border shadow-lg" onClick={() => reset()}>Try again</button>
+      </div>
+    </main>
   )
 }
 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,12 +7,12 @@ export const metadata = {
 
 const NotFoundPage = () => {
   return (
-    <div className="page-container">
-      <main>
-        <h2>404 - Not Found</h2>
-        <Link href="/">Back to listing</Link>
-      </main>
-    </div>
+    <main>
+      <div className="flex flex-col gap-4 items-center bg-white py-10">
+        <h2 className="mx-1 my-2 shrink font-serif text-3xl font-extrabold leading-tight">404 - Not Found</h2>
+        <Link className="bg-gray-800 hover:bg-gray-600 focus:ring focus:ring-gray-400 ring-inset text-white font-serif lowercase p-3 box-border shadow-lg" href="/">Back to courselisting</Link>
+      </div>
+    </main>
   )
 }
 

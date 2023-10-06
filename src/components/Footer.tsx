1d7e76b81e36152fe1688270395deb0018d79215
Tuomas Alanen <tuomas@alanen.dev>
2023-10-06 19:02:04 +0300

    Convert footer to grid layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 shadow-inner">
       <div className="container mx-auto box-border flex max-w-screen-lg flex-col flex-wrap gap-y-5 px-5 py-8 text-white">
-        <div className="flex flex-col">
-          <p>
+        <div className="footer-container">
+          <div className="footer-name">
             <span className="font-serif text-lg font-bold uppercase">
               Tärpistö
-            </span>{' '}
-            - The TKO-äly ry exam archive.
-          </p>
-          <p className="inline-flex">
+            </span>
+            <span>The TKO-äly ry exam archive.</span>
+          </div>
+          <p className="footer-email">
             <EnvelopeIcon className="me-2 h-4 w-4 self-center stroke-white" />
             <span className="sr-only">Contact:</span>
             <a
@@ -23,16 +23,11 @@ const Footer = () => {
               tarpisto@tko-aly.fi
             </a>
           </p>
-        </div>
-        <div
-          className="flex flex-col divide-x-0 divide-y sm:flex-row sm:divide-x sm:divide-y-0"
-          data-test-id="footer-links"
-        >
           <Link
             rel="noopener"
             target="_blank"
             href="https://www.tko-aly.fi/"
-            className="py-2 hover:underline hover:decoration-cyan-500 sm:py-0 sm:pe-3"
+            className="footer-tkoaly hover:underline hover:decoration-cyan-500"
           >
             TKO-äly ry
           </Link>
@@ -40,7 +35,7 @@ const Footer = () => {
             rel="noopener"
             target="_blank"
             href="https://www.tko-aly.fi/tietosuoja"
-            className="py-2 hover:underline hover:decoration-cyan-500 sm:px-3 sm:py-0"
+            className="footer-privacy hover:underline hover:decoration-cyan-500"
           >
             Privacy
           </Link>
@@ -48,7 +43,7 @@ const Footer = () => {
             rel="nofollow noopener"
             target="_blank"
             href="https://github.com/TKOaly/exam-archive-new/"
-            className="py-2 hover:underline hover:decoration-cyan-500 sm:px-3 sm:py-0"
+            className="footer-sourcecode hover:underline hover:decoration-cyan-500"
           >
             Source code
           </Link>
@@ -56,7 +51,7 @@ const Footer = () => {
             rel="noopener"
             target="_blank"
             href="https://fuksiwiki.tko-aly.fi/T%C3%A4rpist%C3%B6"
-            className="py-2 hover:underline hover:decoration-cyan-500 sm:py-0 sm:ps-3"
+            className="footer-fuksiwiki hover:underline hover:decoration-cyan-500"
           >
             Fuksiwiki
           </Link>

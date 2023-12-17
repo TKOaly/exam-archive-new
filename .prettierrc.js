8399d021f215f23870cf443555a2cee4212bdc96
Tuomas Alanen <tuomas@alanen.dev>
2023-12-17 12:16:35 +0200

    Fix prettier configuration invalid in VSCode

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: false,
-  singleQuote: true,
-  tabWidth: 2,
-  printWidth: 80,
-  arrowParens: 'avoid',
-  trailingComma: 'none',
-  plugins: [import('prettier-plugin-tailwindcss')]
-}

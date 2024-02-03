6d1af3c4cce1281d9fdf0dc6c0b3f9244f042134
Tuomas Alanen <tuomas@alanen.dev>
2024-02-03 21:14:27 +0200

    Small accessibility fixes

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -17,7 +17,10 @@ const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
 
   return (
     <main>
-      <div className="m-5 flex flex-col items-center gap-4 bg-gray-50 px-20 py-10">
+      <div
+        role="alert"
+        className="m-5 flex flex-col items-center gap-4 bg-gray-50 px-20 py-10"
+      >
         <h2 className="mx-1 my-2 shrink font-serif text-3xl font-extrabold leading-tight">
           Something went wrong!
         </h2>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,8 +7,11 @@ export const metadata = {
 
 const NotFoundPage = () => {
   return (
-    <div role="alert">
-      <div className="m-5 flex flex-col items-center gap-4 bg-gray-50 px-20 py-10">
+    <main>
+      <div
+        role="alert"
+        className="m-5 flex flex-col items-center gap-4 bg-gray-50 px-20 py-10"
+      >
         <h2 className="mx-1 my-2 shrink font-serif text-3xl font-extrabold leading-tight">
           404 - Not Found
         </h2>
@@ -19,7 +22,7 @@ const NotFoundPage = () => {
           Back to courselisting
         </Link>
       </div>
-    </div>
+    </main>
   )
 }
 

0d6cffab0e7c6f591f6d1649f8495894f3d13729
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2024-04-10 13:52:15 +0300

    Remove development environment specific S3 configuration
    Both production and development environments now use MinIO and so we can
    use MinIO specific settings in both environments.

--- src/service/s3.ts
+++ src/service/s3.ts
@@ -3,20 +3,8 @@ import config from '../config'
 
 const s3Options: S3ClientConfig = {
   apiVersion: '2006-03-01',
-  region: config.AWS_REGION
-}
-
-if (config.NODE_ENV === 'development') {
-  s3Options.credentials = {
-    accessKeyId: config.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: config.AWS_SECRET_ACCESS_KEY!
-  }
-  if (config.AWS_S3_ENDPOINT) {
-    s3Options.endpoint = config.AWS_S3_ENDPOINT
-  }
-  if (config.AWS_S3_FORCE_PATH_STYLE) {
-    s3Options.forcePathStyle = true
-  }
+  region: config.AWS_REGION,
+  forcePathStyle: true
 }
 
 const s3 = new S3(s3Options)

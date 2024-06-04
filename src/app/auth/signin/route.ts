9f97e83a94914e5da60b2f1d7459fa1993cee848
Tuomas Alanen <tuomas@alanen.dev>
2024-06-04 19:12:16 +0300

    Fix login/logout page not loading styles by replacing it

--- src/app/auth/signin/route.ts
+++ src/app/auth/signin/route.ts
@@ -1,5 +1,32 @@
-import { redirect } from 'next/navigation'
+import config from '@lib/config'
+
+export const dynamic = 'force-dynamic'
 
 export const GET = async (req: Request) => {
-  redirect('/api/auth/signin')
+  const csrfResponse = await fetch(`${config.NEXTAUTH_URL}/api/auth/csrf`, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  const csrf = await csrfResponse.json()
+
+  const authResponse = await fetch(
+    `${config.NEXTAUTH_URL}/api/auth/signin/tkoaly`,
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Cookie: csrfResponse.headers
+          .getSetCookie()
+          .reduce((prev, next) => prev + next.split(';')[0] + '; ', '')
+      },
+      body: `csrfToken=${csrf.csrfToken}&callbackUrl=http%3A%2F%2F127.0.0.1%3A9000%2F&json=true`,
+      method: 'POST'
+    }
+  )
+  const auth = await authResponse.json()
+
+  const headers = new Headers(authResponse.headers)
+  headers.set('Location', auth.url)
+
+  return new Response(null, { status: 302, headers: headers })
 }
--- src/app/auth/signout/route.ts
+++ src/app/auth/signout/route.ts
@@ -1,5 +1,32 @@
-import { redirect } from 'next/navigation'
+import config from '@lib/config'
+import { cookies } from 'next/headers'
 
 export const GET = async (req: Request) => {
-  redirect('/api/auth/signout')
+  const cookiesIn = await cookies()
+
+  const csrfResponse = await fetch(`${config.NEXTAUTH_URL}/api/auth/csrf`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Cookie: cookiesIn.toString()
+    }
+  })
+  const csrf = await csrfResponse.json()
+
+  const authResponse = await fetch(
+    `${config.NEXTAUTH_URL}/api/auth/signout/tkoaly`,
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Cookie: cookiesIn.toString()
+      },
+      body: `csrfToken=${csrf.csrfToken}&callbackUrl=http%3A%2F%2F127.0.0.1%3A9000%2F&json=true`,
+      method: 'POST'
+    }
+  )
+  const auth = await authResponse.json()
+
+  const headers = new Headers(authResponse.headers)
+  headers.set('Location', auth.url)
+
+  return new Response(null, { status: 302, headers: headers })
 }
